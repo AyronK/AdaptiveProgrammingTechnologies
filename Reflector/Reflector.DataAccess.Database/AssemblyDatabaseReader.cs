dd57f73d3ad07d966383028784dc3cb58fa6198c
Ayron <Ayron@AYRON-LAPTOP>
2017-10-05 08:13:30 +0200

    Added database accessors library

---
+++ Reflector/Reflector.DataAccess.Database/AssemblyDatabaseReader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Reflector.Models;
+
+namespace Reflector.DataAccess.Database
+{
+    public class AssemblyDatabaseReader : IAssemblyReader
+    {
+        public AssemblyInfo Read()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Reflector/Reflector.DataAccess.Database/AssemblyDatabaseWriter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Reflector.Models;
+
+namespace Reflector.DataAccess.Database
+{
+    public class AssemblyDatabaseWriter : IAssemblyWriter
+    {
+        public void Write(AssemblyInfo assemblyInfo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
