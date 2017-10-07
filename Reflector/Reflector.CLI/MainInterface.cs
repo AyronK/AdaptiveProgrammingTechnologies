56b148f0bef5bba74245f39c583585a6cb965331
AyronKotlet <AyronKotlet@users.noreply.github.com>
2017-10-07 11:16:48 +0200

    CLI initialization

---
+++ Reflector/Reflector.CLI/MainInterface.cs
@@ -0,0 +1,32 @@
+﻿using Reflector.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflector.CLI
+{
+    public class MainInterface
+    {
+        IDataAccessor dataAccessor;
+        public MainInterface(IDataAccessor dataAccessor)
+        {
+            this.dataAccessor = dataAccessor;
+        }
+
+        public void Start()
+        {
+            if(dataAccessor != null)
+            {
+                Console.WriteLine("Successfully loaded data accessor");
+            }
+            else
+            {
+                Console.WriteLine("Error while loading data accessor");
+            }
+
+            Console.Read();
+        }
+    }
+}
--- Reflector/Reflector.CLI/Program.cs
+++ Reflector/Reflector.CLI/Program.cs
@@ -1,5 +1,11 @@
-﻿using System;
+﻿using Microsoft.Practices.Unity;
+using Reflector.DataAccess;
+using Reflector.DataAccess.Dll;
+using Reflector.DataAccess.Xml;
+using Reflector.Logic;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +16,36 @@ namespace Reflector.CLI
     {
         static void Main(string[] args)
         {
+            var readerType = ConfigurationManager.AppSettings["AssemblyReader"];
+            var writerType = ConfigurationManager.AppSettings["AssemblyWriter"];
+
+            IUnityContainer container = new UnityContainer();
+
+            // Readers
+            switch (readerType)
+            {
+                case "DLL":
+                    container.RegisterType<IAssemblyReader, AssemblyDllReader>();
+                    break;
+                case "XML":
+                    container.RegisterType<IAssemblyReader, AssemblyXmlDeserializer>();
+                    break;
+                default:
+                    throw new ConfigurationErrorsException("Not supported reader");
+            }
+
+            // Writers
+            switch (writerType)
+            {
+                case "XML":
+                    container.RegisterType<IAssemblyWriter, AssemblyXmlSerializer>();
+                    break;
+                default:
+                    throw new ConfigurationErrorsException("Not supported reader");
+            }
+
+            container.RegisterType<IDataAccessor, DataAccessor>();
+            container.Resolve<MainInterface>().Start();
         }
     }
 }
