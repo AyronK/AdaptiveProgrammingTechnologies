d3fe50ec4a7e3849fb1bd0390242ef15d833ff96
Beata <Beata@LENOVO-PC>
2017-10-18 16:19:49 +0200

    works

--- Reflector/Reflector.CLI/MainInterface.cs
+++ Reflector/Reflector.CLI/MainInterface.cs
@@ -29,36 +29,35 @@ namespace Reflector.CLI
 
             //Console.WriteLine("Type the name of the file: ");
             //String name = Console.Read().ToString();
-            String name = "Reflector.DataAccess.dll";
-            String path = System.IO.Directory.GetCurrentDirectory() + "\\" + name;
+            // String name = "Reflector.DataAccess.dll";
+            //String path = System.IO.Directory.GetCurrentDirectory() + "\\" + name;
+            String path = "C://Users//Beata//Desktop//sem V//TPA//Reflector//RecursiveLibrary//bin//Debug//ABC.dll";
             AssemblyInfo assemblyInfo = dataAccessor.LoadAssembly(path);
-            string nodeKey = "1";
+            string nodeKey = "1", previousNode = "-1";
             
             TreeLevelModel tree = new TreeLevelModel(assemblyInfo);
             tree._currentLevel.IsExpanded = true;
+          
 
             do
             {
                 Description();
 
-                while (nodeKey != "Q")
-                {
-                    tree.ShowTree(tree._currentLevel, nodeKey, nodeKey.Length);
-                    break;
-                }
+                if(previousNode!=nodeKey) tree.ShowTree(tree._currentLevel, nodeKey, previousNode, nodeKey.Length);
 
                 nodeKey = Console.ReadLine();
                 Console.Clear();
             }
             while (nodeKey != "Q");
-
+            previousNode = nodeKey;
             Console.WriteLine("Po wyjsciu z petli");
             Console.Read();
         }
 
         private void Description()
         {
-            Console.WriteLine("\nWrite unique code of the node or press \"Q\" to break the program");
+            Console.WriteLine("\nWrite unique code of the node\n" +
+                                "Press \"Q\" to break the program\n");
             Console.WriteLine("#################################################################\n");
         }
     }
--- Reflector/Reflector.CLI/TreeLevelModel.cs
+++ Reflector/Reflector.CLI/TreeLevelModel.cs
@@ -20,91 +20,95 @@ namespace Reflector.CLI
         public TreeLevel _currentLevel { get; set; }
         public Dictionary<string, TreeLevel> _sublevel { get; set; }
 
-
-        private void ExpandLevel(TreeLevel currentLevel, string node)
+        private void ExpandLevel(TreeLevel currentLevel, string node, string previousNode)
         {
+           string showableNode = node;
+            if (currentLevel.IsExpanded == true && node!=previousNode)
+            {
+                showableNode = showableNode.TrimEnd(showableNode[showableNode.Length - 1]);
+            }
             currentLevel.IsExpanded = true;
 
 
             if (currentLevel != _firstLevel)
             {
-                for (int i = 0; i <= node.Length; i = i + 2)
+                for (int i = 0; i < showableNode.Length; i = i + 2)
                 {
-                    node = node.Insert(i, "|");
+                    showableNode = showableNode.Insert(i, "|");
                 }
             }
             else
             {
-                node = null;
+                showableNode = null;
             }
 
             foreach (var n in currentLevel.Sublevel)
             {
-                Console.WriteLine($"{node}|{n.Key}| {n.Value.Name}");
+                Console.WriteLine($"{showableNode}|{n.Key}| {n.Value.Name}");
             }
         }
 
-        private void ShrinkLevel(TreeLevel currentLevel, string node)
-        {
-            currentLevel.IsExpanded = false;
-            if (node.Length > 1)
-            {
-                string previousKey = node.TrimEnd(node[node.Length - 1]);
-                ExpandLevel(currentLevel, previousKey);
-            }          
-        }
+        /* private void ShrinkLevel(TreeLevel currentLevel, string node)
+         {
+             currentLevel.IsExpanded = false;
+             if (node.Length > 1)
+             {
+                 string previousKey = node.TrimEnd(node[node.Length - 1]);
+                 ExpandLevel(currentLevel, previousKey);
+             }          
 
-        public void ShowTree(TreeLevel currentLevel, string node, int iterator)
+           if (currentLevel != _firstLevel)
+             {
+                 foreach (var s in currentLevel.Sublevel)
+                 {
+                     ShrinkLevel(s.Value, node);
+                 }
+             }
+
+         } */
+
+        public void ShowTree(TreeLevel currentLevel, string node, string previousNode, int iterator)
         {
-            if(iterator>1)
+            var sublevel = currentLevel.Sublevel;
+            if (currentLevel == _firstLevel && node == "1")
             {
-                if (currentLevel.IsExpanded)
+                ExpandLevel(currentLevel, node, previousNode);
+            }
+            else
+            {
+
+
+                if (iterator > 1)
                 {
-                    //0 poziom jest, każdy kolejny expand + wyswietl w tej metodzie
-                    ExpandLevel(currentLevel, node);
-
-                    foreach (var sublevelkey in _sublevel.Keys)
+                    if (currentLevel.IsExpanded)
                     {
-                        currentLevel = _sublevel[sublevelkey];
-                        if(sublevelkey == node)
-                      //  currentLevel.IsExpanded = true;
-                        ShowTree(currentLevel, node, iterator);
+                     
+                        ExpandLevel(currentLevel, node, previousNode);
 
+                        foreach (var sublevelkey in sublevel.Keys)
+                        {
+                            currentLevel = sublevel[sublevelkey];
+                            if (sublevelkey == node[node.Length - iterator].ToString())
+                                ShowTree(currentLevel, node, previousNode, iterator-1);
+                        }
+                        iterator--;
                     }
-
                 }
-                iterator--;
-            }
-
-               
-
 
                 if (iterator == 1)
                 {
-
-                    //ostatnie przejscie, porównać node z kluczem sublevelu i tylko ten expandnąć
-                    foreach(var finalLevelKey in currentLevel.Sublevel.Keys)
+                    foreach (var finalLevelKey in currentLevel.Sublevel.Keys)
                     {
                         var finalLevel = currentLevel.Sublevel[finalLevelKey];
-                        if (finalLevelKey == node[node.Length-1].ToString())
+                        if (finalLevelKey == node[node.Length - 1].ToString())
                         {
-                            if (finalLevel.IsExpanded == true)
-                            {
-                                ShrinkLevel(finalLevel, node);
-                                Console.WriteLine("chowam wiersz o numerze " + node);
-                            }
-                            else
-                            {
-                                ExpandLevel(finalLevel, node);
-                                Console.WriteLine("rozwijam wiersz o numerze " + node);
-                            }
-                        } 
+                            ExpandLevel(currentLevel, node, previousNode);
+                            ExpandLevel(finalLevel, node, previousNode);
+                            Console.WriteLine("rozwijam wiersz o numerze " + node);
+                        }
                     }
                 }
-
             }
-
+        }
     }
-
-
 }
